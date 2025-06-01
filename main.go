83e1959385f4a40963c55c5f43edd0b83a9a17c0
Abhinav Sinha <abhisinha0601@gmail.com>
2025-06-01 17:18:27 +0100

    Refactored the code and seperated the model into a package

--- main.go
+++ main.go
@@ -1,50 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+
+	"github.com/abhisinha2001/weathercli/models"
 )
-
-type weatherModel struct {
-	Name string `json:"name`
-	Main struct {
-		Temp float64 `json:"temp"`
-	} `json:"temp`
-	Weather []struct {
-		Description string `json:"description`
-	} `json:"weather"`
-}
-
-func getWeather(city, apiKey string) (*weatherModel, error) {
-	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, apiKey)
-
-	resp, err := http.Get(url)
-
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("Failed to get weather: status code %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		return nil, err
-	}
-	var weatherResp weatherModel
-	if err := json.Unmarshal(body, &weatherResp); err != nil {
-		return nil, err
-	}
-	return &weatherResp, nil
-}
 
 func main() {
 
@@ -68,7 +31,7 @@ func main() {
 	city := os.Args[1]
 	fmt.Printf("Fetching weather for %s\n", city)
 
-	weather, err := getWeather(city, apiKey)
+	weather, err := models.GetWeather(city, apiKey)
 
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
